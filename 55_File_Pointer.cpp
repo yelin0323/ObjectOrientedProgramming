3a4375f178a6929818d6b4793a235ba21b392975
yelin0323 <48678537+yelin0323@users.noreply.github.com>
2020-11-23 12:52:50 +0900

    Create 55_File_Pointer.cpp

---
+++ 55_File_Pointer.cpp
@@ -0,0 +1,55 @@
+/*
+	move file pointer
+	 - file이 어디를 읽고 있는지 가리키는 pointer
+
+	std::ios::beg
+	std::ios::end
+	std::ios::cur
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	/*
+	int data[]{ 10,20,30,40,50 };
+
+	std::ofstream output_f("bdata.bin", std::ios::binary);
+
+	if (output_f.fail() == true)
+	{
+		exit(1);
+	}
+
+	output_f.write((char*)data, sizeof(int) * 5);
+	output_f.close();
+	*/
+
+	std::ifstream input_f("bdata.bin", std::ios::binary);
+
+	if (input_f.fail() == true)
+	{
+		exit(1);
+	}
+
+	int k;
+	//input_f.read((char*)& k, sizeof(int));
+
+	//input_f.seekg(sizeof(int) * 2, std::ios::beg);
+	input_f.seekg(-1* (sizeof(int) * 3), std::ios::end);
+	//input_f.seekg(sizeof(int) * 2, std::ios::cur); //cur == beg;
+
+	input_f.read((char*)& k, sizeof(int));
+	std::cout << "Value: " << k << endl;
+
+	//input_f.seekg(sizeof(int) *-1, std::ios::cur); //cur == beg;
+
+	input_f.read((char*)& k, sizeof(int));
+	std::cout << "Value: " << k << endl;
+
+	return 0;
+}
